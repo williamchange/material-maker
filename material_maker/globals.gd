bb623a11e35c9fbbea5561acbad36468fa840aa0
LMW <williamchange@gmail.com>
2026-03-02 21:54:10 +0800

    Fixed inconsistent popup size under graph zoom

--- material_maker/globals.gd
+++ material_maker/globals.gd
@@ -175,9 +175,12 @@ func popup_menu(menu : PopupMenu, parent : Control):
 	var zoom_fac = 1.0
 	if parent is GraphNode:
 		zoom_fac *= mm_globals.main_window.get_current_graph_edit().zoom
-	
-	var content_scale_factor = mm_globals.main_window.get_window().content_scale_factor
-	menu.popup(Rect2(parent.get_local_mouse_position()*content_scale_factor*zoom_fac + parent.get_screen_position(), Vector2(0, 0)))
+
+	var csf = get_tree().root.content_scale_factor
+	menu.get_window().content_scale_factor = csf
+	menu.position = parent.get_local_mouse_position()*csf*zoom_fac + parent.get_screen_position()
+	menu.get_window().transient = true
+	menu.show()
 
 func set_tip_text(tip : String, timeout : float = 0.0, priority: int = 0):
 	if main_window:
